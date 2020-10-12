198c5ab356cea3e5e32907dcfe534f70a23bb258
Lindsay <lindsaynadell89@gmail.com>
2020-10-12 17:14:11 -0400

    Room Calculator

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RoomCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Circus Room Detail Generator!");
+
+            string cont = "y";
+
+            while (cont.ToLower() == "y")
+            {
+                Console.WriteLine("Enter Length:");
+                double length = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Width:");
+                double width = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Height:");
+                double height = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Area: {length * width}");
+
+                Console.WriteLine($"Perimeter: {2* (length + width)}");
+
+                Console.WriteLine($"Volume: {length * width * height}");
+
+                Console.WriteLine("Continue? (y/n)");
+                cont = Console.ReadLine();
+            }
+            Console.WriteLine("Have a great day!");
+        }
+    }
+}
